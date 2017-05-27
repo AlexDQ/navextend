69f3413b0a83b38e72d687f879763b18039d1e96
AlexDQ <499233891@qq.com>
2017-05-27 14:52:28 +0800

    modify

---
+++ jQuery-nav.js
@@ -0,0 +1,24 @@
+;(function(){
+	
+	$.fn.extend({
+		"nav":function(color,background){
+			$(this).find(".nav").css({
+				"list-style":"none",
+				"margin":0,
+				"padding":0,
+				"display":"none",
+				"color":color,	
+				"background":background
+			})
+			$(this).find(".nav").parent().hover(function(){
+				$(this).find(".nav").slideDown()
+			},function(){
+				$(this).find(".nav").stop(true).slideUp()
+			})
+			return this;
+		}
+	});
+	
+})(jQuery);
+
+
